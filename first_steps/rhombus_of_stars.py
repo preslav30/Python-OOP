bf366f7cc9a928f329f10f9dfb9aa7f42a04490c
preslav30 <112109330+preslav30@users.noreply.github.com>
2023-02-28 16:02:49 +0200

    created new folder

---
+++ first_steps/rhombus_of_stars.py
@@ -0,0 +1,13 @@
+def each_row(size, star_count):
+    for row in range(size - star_count):
+        print(' ', end='')
+    for row in range(1, star_count):
+        print('*', end=' ')
+    print('*')
+
+size = int(input())
+for star_count in range(1, size):
+    each_row(size, star_count)
+for star_count in range(size, 0, -1):
+    each_row(size, star_count)
+
